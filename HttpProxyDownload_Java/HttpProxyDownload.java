7d20cbb68697ba5db99625b5283a8ce919cbc2a8
Raghu <raghu15sep99@gmail.com>
2020-04-11 12:03:11 +0530

    Add: proxy download class in java

--- HttpProxyDownload_Java/HttpProxyDownload.java
+++ HttpProxyDownload_Java/HttpProxyDownload.java
@@ -1,11 +1,20 @@
-import java.net.*;
+/* BeginGroupMembers */
+/* f20170703@hyderabad.bits-pilani.ac.in B Raghunathan */
+/* f20171573@hyderabad.bits-pilani.ac.in Gunpreet Kaur */
+/* f20170325@hyderabad.bits-pilani.ac.in Nihal Jain */
+/* f20170297@hyderabad.bits-pilani.ac.in S Ankit */
+/* EndGroupMembers */
+
+/* Program to access and download content from websites through a proxy server using socket level requests */
+
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.io.*;
 import java.util.Base64;
 import javax.net.ssl.*;
 import java.nio.charset.StandardCharsets;
 import java.util.regex.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 class HttpProxyDownload {
 
@@ -15,10 +24,14 @@ class HttpProxyDownload {
 
     /**
      * Initialize class variables using commandline args
+     * 
      * @param argv
      */
     public HttpProxyDownload(String[] argv) {
         url = argv[0];
+        if(!url.substring(0, 4).equals("www.")) {
+            url = "www." + url;
+        }
         ip = argv[1];
         username = argv[3];
         password = argv[4];
@@ -29,9 +42,10 @@ class HttpProxyDownload {
 
     /**
      * Makes a HTTP GET request and writes the data content to file
-     * @param get   URL for GET
-     * @param filename  filepath to save response data
-     * @return  1: on sucess, 0: error
+     * 
+     * @param get      URL for GET
+     * @param filename filepath to save response data
+     * @return 1: on sucess, 0: error
      */
     public int makeRequest(String get, String filename) {
         try {
@@ -41,22 +55,29 @@ class HttpProxyDownload {
             String encodedKey = username + ":" + password;
             encodedKey = Base64.getEncoder().encodeToString(encodedKey.getBytes("utf-8"));
             String address = url + ":" + 443;
-            String req = "CONNECT " + address + " HTTP/1.1\r\n" + "host: " + address + "\r\n"
-                    + "proxy-connection: keep-alive\r\n" + "proxy-authorization: Basic " + encodedKey + "\r\n" + "\r\n";
-
+            String req = 
+                "CONNECT " + address + " HTTP/1.1\r\n" + 
+                "host: " + address + "\r\n" + 
+                "proxy-connection: keep-alive\r\n" + 
+                "proxy-authorization: Basic " + encodedKey + "\r\n" + 
+                "\r\n";
             // Send CONNECT request
-            System.out.print(req);
+            // System.out.print(req);
             OutputStream out = sock.getOutputStream();
             out.write(req.getBytes(StandardCharsets.UTF_8));
 
             // Response from proxy
             BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            System.out.println(in.readLine());
-            System.out.print(in.readLine());
+            in.readLine();
+            in.readLine();
 
-            req = "GET " + get + " HTTP/1.1\r\n" + "host: " + url + "\r\n" + "connection: close\r\n\r\n";
+            req = 
+                "GET " + get + " HTTP/1.1\r\n" + 
+                "host: " + url + "\r\n" + 
+                "connection: close\r\n" + 
+                "\r\n";
 
-            System.out.print(req);
+            // System.out.print(req);
             // Wrap using SSL for HTTPS connection
             SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
             SSLSocket sslsock = (SSLSocket) factory.createSocket(sock, sock.getInetAddress().getHostAddress(),
@@ -89,7 +110,7 @@ class HttpProxyDownload {
                     fout.write(r);
                 } else {
                     // Header section
-                    System.out.print((char) r);
+                    // System.out.print((char) r);
                 }
                 for (int i = 0; i < 3; i++)
                     prev[i] = prev[i + 1];
@@ -144,7 +165,7 @@ class HttpProxyDownload {
             // handle relative path image URLs
             if (!imgsrc.substring(0, 6).equals("https:"))
                 imgsrc = "/" + imgsrc;
-            System.out.println(imgsrc);
+            // System.out.println(imgsrc);
             // GET logo
             hpd.makeRequest(imgsrc, hpd.getflogo());
         }
